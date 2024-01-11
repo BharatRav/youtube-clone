32547a1f26632834412f7668e0fc529551772e78
Bharat Rav <bharatrav8077@gmail.com>
2024-01-11 23:07:51 +0530

    rapid api setup and url setup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { AppContext } from "./context/contextApi.js";
+
 function App() {
-  return <div className="App">hello</div>;
+  return (
+    <AppContext>
+      <BrowserRouter>
+        <div className="flex flex-col h-full">
+          <Routes>
+            <Route path="/" exact element={"feed"} />
+            <Route path="/searchResult/:searchQuery" element={"search"} />
+            <Route path="/video/:id" element={"video"} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </AppContext>
+  );
 }
 
 export default App;
