defec9dac2be7bb8ad78b6326a4b5a77243b5915
Bharat Rav <bharatrav8077@gmail.com>
2024-01-12 09:56:45 +0530

    loader videolength and search results for video navbar and header have been completed

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { AppContext } from "./context/contextApi.js";
+import Header from "./components/Header.js";
+import SearchResult from "./components/SearchResult.js";
 
 function App() {
   return (
     <AppContext>
       <BrowserRouter>
-        <div className="flex flex-col h-full">
+        <div className="flex flex-col h-full bg-red-300">
+          <Header />
           <Routes>
             <Route path="/" exact element={"feed"} />
-            <Route path="/searchResult/:searchQuery" element={"search"} />
+            <Route
+              path="/searchResult/:searchQuery"
+              element={<SearchResult />}
+            />
             <Route path="/video/:id" element={"video"} />
           </Routes>
         </div>
