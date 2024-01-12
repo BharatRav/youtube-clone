cd2b4c2fad7fd5d80111aa173553f084456c1200
Bharat Rav <bharatrav8077@gmail.com>
2024-01-12 11:28:50 +0530

    feed card  created

--- src/App.js
+++ src/App.js
@@ -3,15 +3,16 @@ import { AppContext } from "./context/contextApi.js";
 import Header from "./components/Header.js";
 import SearchResult from "./components/SearchResult.js";
 import VideoDetails from "./components/VideoDetails.js";
+import Feed from "./components/Feed.js";
 
 function App() {
   return (
     <AppContext>
       <BrowserRouter>
-        <div className="flex flex-col h-full bg-red-300">
+        <div className="flex flex-col h-full">
           <Header />
           <Routes>
-            <Route path="/" exact element={"feed"} />
+            <Route path="/" exact element={<Feed />} />
             <Route
               path="/searchResult/:searchQuery"
               element={<SearchResult />}
